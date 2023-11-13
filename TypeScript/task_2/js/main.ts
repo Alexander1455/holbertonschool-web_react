30726f897c03fcbd99b01c3b44be55f051a6d544
Alexander1455 <alexander21111121@gmail.com>
2023-11-12 21:03:44 -0500

    Advanced types

---
+++ TypeScript/task_2/js/main.ts
@@ -0,0 +1,60 @@
+// task_2/js/main.ts
+
+// Define the DirectorInterface with the required methods
+interface DirectorInterface {
+    workFromHome(): string;
+    getToWork(): string;
+    workDirectorTasks(): string;
+  }
+  
+  // Define the TeacherInterface with the required methods
+  interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+  }
+  
+  // Implement the Director class that implements DirectorInterface
+  class Director implements DirectorInterface {
+    workFromHome(): string {
+      return "Working from home";
+    }
+  
+    getToWork(): string {
+      return "Getting a coffee break";
+    }
+  
+    workDirectorTasks(): string {
+      return "Getting to director tasks";
+    }
+  }
+  
+  // Implement the Teacher class that implements TeacherInterface
+  class Teacher implements TeacherInterface {
+    workFromHome(): string {
+      return "Cannot work from home";
+    }
+  
+    getCoffeeBreak(): string {
+      return "Cannot have a break";
+    }
+  
+    workTeacherTasks(): string {
+      return "Getting to work";
+    }
+  }
+  
+  // Create a function createEmployee that returns either a Director or a Teacher instance
+  function createEmployee(salary: number | string): Director | Teacher {
+    if (typeof salary === "number" && salary < 500) {
+      return new Teacher();
+    } else {
+      return new Director();
+    }
+  }
+  
+  // Examples
+  console.log(createEmployee(200));   // Teacher
+  console.log(createEmployee(1000));  // Director
+  console.log(createEmployee('$500')); // Director
+  
